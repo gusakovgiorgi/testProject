923134f662ad504fe7ef060f4ec3ab1fe1141811
gusa <gusakov.giorgi@gmail.com>
2016-10-10 07:41:58 +0300

    92% i think main functions realeased

--- app/src/main/java/net/gusakov/testprojectexample/adapters/ListViewAdapter.java
+++ app/src/main/java/net/gusakov/testprojectexample/adapters/ListViewAdapter.java
@@ -13,6 +13,8 @@ import android.graphics.Point;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,8 +22,11 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 import net.gusakov.testprojectexample.Gallery_activity;
 import net.gusakov.testprojectexample.R;
@@ -37,12 +42,18 @@ public class ListViewAdapter extends BaseAdapter {
     LayoutInflater lInflater;
     Integer[] objects;
     private int screenWidth;
+    private int screenHeight;
+    private float screenDensity;
     private final int[] mImage = {R.drawable.tmp_poster1, R.drawable.tmp_poster2};
+    private final int[] mAlbumImage={R.drawable.tmp_album_1,R.drawable.tmp_album_2};
     public static final int KEY_IMAGE_VIEW_TAG = -100;
     private int previousViewWidth;
     private int previousViewHeight;
     public static final String KEY_GALLERY_ACTIVITY_INTENT_EXTRA_IMAGES_ARRAY = "GALLERY_ACTIVITY_INTENT_EXTRA_IMAGES_ARRAY";
     public static final String KEY_GALLERY_ACTIVITY_INTENT_EXTRA_IMAGE_POSITION = "GALLERY_ACTIVITY_INTENT_EXTRA_IMAGE_POSITION";
+    public static final int ALBUM_IMAGE_FIRST_TEMPLATE_RESOURCE_ID=R.drawable.first_album_template;
+    public static final int ALBUM_IMAGE_SECOND_TEMPLATE_RESOURCE_ID=R.drawable.second_album_template;
+    boolean firstImageTemplateIsUsed=false;
 
 
     public ListViewAdapter(Context context, Integer[] images) {
@@ -52,9 +63,13 @@ public class ListViewAdapter extends BaseAdapter {
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         WindowManager wm = (WindowManager) ctx.getSystemService(Context.WINDOW_SERVICE);
         Display display = wm.getDefaultDisplay();
+        DisplayMetrics dm=new DisplayMetrics();
+        display.getMetrics(dm);
         Point size = new Point();
         display.getSize(size);
         screenWidth = size.x;
+        screenHeight=size.y;
+        screenDensity=dm.density;
 
     }
     @Override
@@ -118,7 +133,15 @@ public class ListViewAdapter extends BaseAdapter {
             });
             return view;
         }else if(position==7) {
-            return returnDrawedView();
+            View view= lInflater.inflate(R.layout.footer_layout,null,false);
+            HorizontalScrollView horizontalScrollView=(HorizontalScrollView)view.findViewById(R.id.footerHorizontalViewId);
+            horizontalScrollView.getLayoutParams().height=(int)(screenHeight/2.5f);
+            LinearLayout linearLayout=(LinearLayout)view.findViewById(R.id.horizontalSVContainertId);
+            for(int i=0;i<mAlbumImage.length;i++){
+                linearLayout.addView(returnDrawedView(mAlbumImage[i]));
+            }
+
+            return view;
         }else{
             ImageView view=null;
             if(convertView instanceof ImageView) {
@@ -136,14 +159,26 @@ public class ListViewAdapter extends BaseAdapter {
         }
     }
 
-    private View returnDrawedView(){
+    private View returnDrawedView(int imageResourceId){
+        int templateid;
+        float angle;
+        if(!firstImageTemplateIsUsed){
+            templateid=ALBUM_IMAGE_FIRST_TEMPLATE_RESOURCE_ID;
+            angle=3.0f;
+            firstImageTemplateIsUsed=true;
+        }else{
+            templateid=ALBUM_IMAGE_SECOND_TEMPLATE_RESOURCE_ID;
+            angle=2.0f;
+            firstImageTemplateIsUsed=false;
+        }
         Bitmap bitmap = BitmapFactory.decodeResource(ctx
-                .getResources(), R.drawable.first_album_template);
+                .getResources(), templateid);
+        Log.v("MyTag","dimensiond: "+bitmap.getWidth()+" x "+bitmap.getHeight());
         /* set other image top of the first icon */
         Bitmap tmpBitMap = BitmapFactory.decodeResource(ctx
-                .getResources(), R.drawable.tmp_album_1);
+                .getResources(), imageResourceId);
         Matrix matrix=new Matrix();
-        matrix.setRotate(2.0f,tmpBitMap.getWidth()/2,tmpBitMap.getHeight()/2);
+        matrix.setRotate(angle,tmpBitMap.getWidth()/2,tmpBitMap.getHeight()/2);
         Bitmap rotatedBitmap = Bitmap.createBitmap(tmpBitMap,0,0,tmpBitMap.getWidth(),tmpBitMap.getHeight(),matrix,true);
 //        rotatedBitmap.eraseColor(Color.TRANSPARENT);
 
@@ -153,12 +188,17 @@ public class ListViewAdapter extends BaseAdapter {
         Canvas canvas = new Canvas(bmOverlay);
 //        canvas.drawARGB(0x00, 0, 0, 0);
         canvas.drawBitmap(bitmap, 0, 0, null);
-        canvas.drawBitmap(rotatedBitmap,80,140,null);
+        canvas.drawBitmap(rotatedBitmap,40*screenDensity,90*screenDensity,null);
         BitmapDrawable dr = new BitmapDrawable(bmOverlay);
         dr.setBounds(0, 0, dr.getIntrinsicWidth(), dr.getIntrinsicHeight());
 
         ImageView imageView=new ImageView(ctx);
+
+//        LinearLayout.LayoutParams params=new LinearLayout.LayoutParams(bmOverlay.getWidth(),bmOverlay.getHeight());
+        Log.v("MyTag","dimensiond: "+bmOverlay.getWidth()+" x "+bmOverlay.getHeight());
+//        imageView.setLayoutParams(params);
                 imageView.setImageDrawable(dr);
+        Log.v("MyTag","imageView dimensions: "+imageView.getWidth()+" x "+imageView.getHeight());
         return imageView;
     }
     private void setScaledImage(ImageView view,int drawableId,float scaleParameter) throws NoSuchElementException {
